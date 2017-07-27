fc2a16a347acc2b069152772980eab47f5b40b4c
Andrew Trask <liamtrask@gmail.com>
2017-07-27 12:18:54 +0100

    added database indexing/tostring/ease-of-use functionality

--- mine/database.py
+++ mine/database.py
@@ -1,13 +1,59 @@
+from os import listdir
+from os.path import isfile, join
+
 class Database(object):
 
     def __init__(self,location):
-        """Proof of concept assumes location == folder with a bunch of .csv files"""
+        """Proof of concept assumes location points to a folder with a bunch of .csv files"""
         self.location = location
+        self.load_datasets()
+
+    def __len__(self):
+        return len(self.datasets)
+
+    def load_datasets(self):
+
+        files = [f for f in listdir(self.location) if (isfile(join(self.location, f)) and f[-4:] == '.csv')]
+
+        self.datasets = {}
+
+        for fname in files:
+            self.datasets[fname] = CSVDataset(self.location+"/"+fname)
+
+        return self.datasets
+
+    def keys(self):
+        return self.datasets.keys()
+
+    def __getitem__(self,i):
+        if(type(i) == int):
+            return list(self.datasets.values())[i]
+        else:
+            return self.datasets[i]
+
+    def __repr__(self):
+        return str(self)
+
+    def __str__(self):
+        return str(list(self.datasets.values()))
 
 
-class Dataset(object):
+class CSVDataset(object):
 
     def __init__(self,location):
-        """Proof of concept assumes location == address to a .csv file"""
+        """Proof of concept assumes location is the full path to a .csv file"""
 
         self.location = location
+        self.name = self.location.split("/")[-1]
+
+    def __len__(self):
+        f = open(self.location,'r')
+        raw = f.readlines()
+        f.close()
+        return len(raw)
+
+    def __repr__(self):
+        return str(self)
+
+    def __str__(self):
+        return "<Dataset:"+str(self.name) + ":"+ str(len(self)) + ">"
