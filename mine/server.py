1845572e5c69090d40c8bbc13ceff169b720e6ff
Andrew Trask <liamtrask@gmail.com>
2017-08-02 05:17:05 +0100

    added flask scheduler

--- mine/server.py
+++ mine/server.py
@@ -1,7 +1,14 @@
-import os
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.triggers.interval import IntervalTrigger
 from mine.database import CSVDataset,Database
 from flask import Flask
+import os
+import time
+import atexit
+
 app = Flask(__name__)
+scheduler = BackgroundScheduler()
+scheduler.start()
 
 @app.route("/")
 def init():
@@ -11,3 +18,16 @@ def init():
 def datasets():
     db = Database(os.environ['OPENMINED_DB_LOC'])
     return db.to_json()
+
+def query_sonar_for_training_opportunities():
+    print("Querying Sonar for Training Opportunities")
+
+scheduler.add_job(
+    func=print_date_time,
+    trigger=IntervalTrigger(seconds=5),
+    id='check_for_training_opportunities',
+    name='Check OpenMined:Sonar for available models to train.',
+    replace_existing=True)
+
+# Shut down the scheduler when exiting the app
+atexit.register(lambda: scheduler.shutdown())
